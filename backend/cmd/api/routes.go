e16eb8dc11fe8283d0f240583e6be06d963a0a53
Ewan Jones <egcjones@gmail.com>
2024-02-24 19:49:53 +0100

    Add requirement for record auth middleware

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cognos-io/chat.cognos.io/backend/internal/chat"
 	"github.com/cognos-io/chat.cognos.io/backend/internal/config"
 	"github.com/cognos-io/chat.cognos.io/backend/pkg/compat/openai"
+	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 	oai "github.com/sashabaranov/go-openai"
 )
@@ -14,7 +15,7 @@ import (
 func addPocketBaseRoutes(app core.App, logger *slog.Logger, config *config.APIConfig, openaiClient *oai.Client, messageRepo chat.MessageRepo) {
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		// https://platform.openai.com/docs/api-reference/chat/create
-		e.Router.POST("/v1/chat/completions", openai.EchoHandler(logger, openaiClient, messageRepo))
+		e.Router.POST("/v1/chat/completions", openai.EchoHandler(logger, openaiClient, messageRepo), apis.RequireRecordAuth())
 		return nil
 	})
 }
