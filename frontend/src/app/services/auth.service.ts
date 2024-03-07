65b5500c08053cc4cdc812d52f1daba4690d5fe7
Ewan Jones <egcjones@gmail.com>
2024-03-07 15:50:51 +0100

    update auth service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import PocketBase, {
+  AuthMethodsList,
+  RecordAuthResponse,
+  RecordModel,
+} from 'pocketbase';
+import { Observable, from } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly authCollection: string = 'users';
+  private readonly pb: PocketBase;
 
-  constructor() { }
+  constructor() {
+    this.pb = new PocketBase(environment.pocketbaseBaseUrl);
+  }
+
+  listAuthMethods(): Observable<AuthMethodsList> {
+    return from(this.pb.collection(this.authCollection).listAuthMethods());
+  }
+
+  loginWithOry(): Observable<RecordAuthResponse<RecordModel>> {
+    return from(
+      this.pb.collection(this.authCollection).authWithOAuth2({
+        // Make sure OIDC provider is configured in PocketBase for Ory
+        provider: 'oidc',
+        scopes: ['openid', 'offline_access'],
+      })
+    );
+  }
+
+  logout(): void {
+    return this.pb.authStore.clear();
+  }
 }
