a538c9db9add9bf87c5b29c0c6679313cc603909
Ewan Jones <egcjones@gmail.com>
2024-03-07 19:53:47 +0100

    begin to add auth state

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -4,15 +4,39 @@ import PocketBase, {
   RecordAuthResponse,
   RecordModel,
 } from 'pocketbase';
-import { Observable, from } from 'rxjs';
+import { Observable, Subject, from } from 'rxjs';
 import { environment } from '../../environments/environment.development';
+import { signalSlice } from 'ngxtension/signal-slice';
+
+export type LoginStatus = 'pending' | 'authenticating' | 'success' | 'error';
+
+export type AuthUser = 
+
+interface AuthState {
+  status: LoginStatus;
+}
+
+const initialState: AuthState = {
+  status: 'pending',
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  // sources
+  login$ = new Subject();
+
   private readonly authCollection: string = 'users';
   private readonly pb: PocketBase;
+
+  // state
+  private state = signalSlice({
+    initialState: initialState,
+  });
+
+  // selectors
+
 
   constructor() {
     this.pb = new PocketBase(environment.pocketbaseBaseUrl);
