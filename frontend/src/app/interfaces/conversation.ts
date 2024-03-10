60f152cf5a65e81a2933cb264f1c6155eea6635b
Ewan Jones <egcjones@gmail.com>
2024-03-10 14:23:32 +0100

    Add Conversation interface with properties and types

--- frontend/src/app/interfaces/conversation.ts
+++ frontend/src/app/interfaces/conversation.ts
@@ -1,2 +1,11 @@
-export interface Conversation {
-}
+import { z } from 'zod';
+import { Base64 } from 'js-base64';
+
+export const Conversation = z.object({
+  id: z.string(),
+  created: z.string().datetime(),
+  updated: z.string().datetime(),
+  data: z.string().refine(Base64.isValid), // base64 encrypted data
+  creatorId: z.string().optional(), // may be null for some reason
+});
+export type Conversation = z.infer<typeof Conversation>;
