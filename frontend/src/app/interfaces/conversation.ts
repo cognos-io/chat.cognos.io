16a375285304b335a15d4bce94c7ba6fc6d42cba
Ewan Jones <egcjones@gmail.com>
2024-03-10 11:33:29 +0100

    begin work on conversation service

---
+++ frontend/src/app/interfaces/conversation.ts
@@ -0,0 +1,2 @@
+export interface Conversation {
+}
--- frontend/src/app/services/conversation.service.ts
+++ frontend/src/app/services/conversation.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+interface ConversationState {}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConversationService {
+  // sources
+  readonly conversation$ = new Subject();
 
-  constructor() { }
+  constructor() {}
 }
