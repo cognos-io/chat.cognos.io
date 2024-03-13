931a3b2f13a3faa9353d7104c5fc593415600505
Ewan Jones <egcjones@gmail.com>
2024-03-13 14:01:23 +0100

    register icon service with bootstrap icons in app

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePocketbase } from './services/pocketbase.service.provider';
+import { IconService } from './services/icon.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     providePocketbase(),
+    IconService,
   ],
 };
