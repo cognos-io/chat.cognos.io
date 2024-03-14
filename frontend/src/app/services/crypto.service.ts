8fc819aea8ad2a22fe9ada6622b78ab4aa5993f4
Ewan Jones <egcjones@gmail.com>
2024-03-14 11:28:22 +0100

    add secretbox functionality to crypto service

--- frontend/src/app/services/crypto.service.ts
+++ frontend/src/app/services/crypto.service.ts
@@ -55,4 +55,43 @@ export class CryptoService {
 
     return decrypted;
   }
+
+  /**
+   * secretBox - Encrypts a message using a secret key and
+   * symmetric encryption.
+   *
+   * @param message (Uint8Array) - The message to encrypt
+   * @param key (Uint8Array) - The secret key
+   * @returns (Uint8Array) - The encrypted message (ciphertext) including the nonce
+   */
+  secretBox(message: Uint8Array, key: Uint8Array): Uint8Array {
+    const nonce = nacl.randomBytes(nacl.secretbox.nonceLength);
+    const ciphertext = nacl.secretbox(message, nonce, key);
+
+    const fullMessage = new Uint8Array(nonce.length + ciphertext.length);
+    fullMessage.set(nonce);
+    fullMessage.set(ciphertext, nonce.length);
+
+    return fullMessage;
+  }
+
+  /**
+   * openSecretBox - Decrypts a message using a secret key and
+   * symmetric encryption.
+   *
+   * @param cipherText (Uint8Array) - The encrypted message (ciphertext) including the nonce
+   * @param key (Uint8Array) - The secret key (Uint8Array)
+   * @returns (Uint8Array) - The decrypted message
+   */
+  openSecretBox(cipherText: Uint8Array, key: Uint8Array): Uint8Array {
+    const nonce = cipherText.slice(0, nacl.secretbox.nonceLength);
+    const ciphertext = cipherText.slice(nacl.secretbox.nonceLength);
+    const decrypted = nacl.secretbox.open(ciphertext, nonce, key);
+
+    if (decrypted === null) {
+      throw new Error('Could not decrypt message');
+    }
+
+    return decrypted;
+  }
 }
