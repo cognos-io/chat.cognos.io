a0592ac7bf6c8dec3adae5cf15e9646cc059bac8
Ewan Jones <egcjones@gmail.com>
2024-03-14 17:57:30 +0100

    add a vault-password-dialog component

---
+++ frontend/src/app/components/vault-password-dialog/vault-password-dialog.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { VaultService } from '../../services/vault.service';
+
+@Component({
+  selector: 'app-vault-password-dialog',
+  standalone: true,
+  imports: [MatButtonModule, ReactiveFormsModule, MatInputModule],
+  templateUrl: './vault-password-dialog.component.html',
+  styleUrl: './vault-password-dialog.component.scss',
+})
+export class VaultPasswordDialogComponent {
+  vaultService = inject(VaultService);
+
+  fb = inject(FormBuilder);
+
+  vaultPasswordForm = this.fb.group({
+    vaultPassword: ['', [Validators.required, Validators.minLength(8)]],
+  });
+}
