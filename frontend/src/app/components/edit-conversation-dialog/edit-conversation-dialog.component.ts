c8df3a9ff0d5e088db250a8d5b5b98a883293855
Ewan Jones <egcjones@gmail.com>
2024-03-16 18:04:40 +0100

    add ability to delete from edit conversation dialog

--- frontend/src/app/components/edit-conversation-dialog/edit-conversation-dialog.component.ts
+++ frontend/src/app/components/edit-conversation-dialog/edit-conversation-dialog.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ConversationService } from '@app/services/conversation.service';
 
 @Component({
   selector: 'app-edit-conversation-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule, MatButtonModule, MatIconModule],
   templateUrl: './edit-conversation-dialog.component.html',
   styleUrl: './edit-conversation-dialog.component.scss',
 })
-export class EditConversationDialogComponent {}
+export class EditConversationDialogComponent {
+  conversationService = inject(ConversationService);
+  data: { conversationId: string } = inject(MAT_DIALOG_DATA);
+}
