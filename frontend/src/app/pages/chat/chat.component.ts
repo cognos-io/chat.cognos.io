55542a3633186144014a623a84fbdc86c1c8cb5a
Ewan Jones <egcjones@gmail.com>
2024-03-16 15:59:55 +0100

    show edit conversation dialog component when clicking on menu for conversation list item

--- frontend/src/app/pages/chat/chat.component.ts
+++ frontend/src/app/pages/chat/chat.component.ts
@@ -3,11 +3,15 @@ import { Component, effect, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
+
+import { EditConversationDialogComponent } from '@app/components/edit-conversation-dialog/edit-conversation-dialog.component';
 
 import { VaultPasswordDialogComponent } from '../../components/vault-password-dialog/vault-password-dialog.component';
 import { ConversationService } from '../../services/conversation.service';
@@ -17,6 +21,7 @@ import { VaultService } from '../../services/vault.service';
   selector: 'app-chat',
   standalone: true,
   imports: [
+    RouterModule,
     MatDialogModule,
     MatToolbarModule,
     MatButtonModule,
@@ -24,6 +29,7 @@ import { VaultService } from '../../services/vault.service';
     MatSidenavModule,
     MatListModule,
     MatTooltipModule,
+    MatExpansionModule,
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
@@ -38,9 +44,6 @@ export class ChatComponent {
     if (!this.vaultService.keyPair()) {
       this.dialogRef = this.dialog.open(VaultPasswordDialogComponent, {
         disableClose: true,
-        backdropClass: 'backdrop-blur',
-        height: '400px',
-        width: '600px',
       });
     } else {
       this.dialogRef?.close();
@@ -57,5 +60,13 @@ export class ChatComponent {
       .observe([Breakpoints.Handset])
       .pipe(takeUntilDestroyed())
       .subscribe((result) => this.isMobile.set(result.matches));
+  }
+
+  onEditConversation(conversationId: string) {
+    this.dialog.open(EditConversationDialogComponent, {
+      data: {
+        conversationId,
+      },
+    });
   }
 }
