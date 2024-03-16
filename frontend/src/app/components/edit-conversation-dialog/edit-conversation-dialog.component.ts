674f70fa84eba84e118efa693f9a769b0d3f9177
Ewan Jones <egcjones@gmail.com>
2024-03-16 15:59:26 +0100

    add placeholder edit conversation dialog component

---
+++ frontend/src/app/components/edit-conversation-dialog/edit-conversation-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-conversation-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-conversation-dialog.component.html',
+  styleUrl: './edit-conversation-dialog.component.scss',
+})
+export class EditConversationDialogComponent {}
