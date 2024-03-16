ec097c4f48986d78deb8718cfd089a8a3f655076
Ewan Jones <egcjones@gmail.com>
2024-03-16 21:08:59 +0100

    update confirmation dialog

--- frontend/src/app/components/confirmation-dialog/confirmation-dialog.component.ts
+++ frontend/src/app/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-confirmation-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule, MatButtonModule, MatIconModule],
   templateUrl: './confirmation-dialog.component.html',
   styleUrl: './confirmation-dialog.component.scss',
 })
-export class ConfirmationDialogComponent {}
+export class ConfirmationDialogComponent {
+  data: { message: string } = inject(MAT_DIALOG_DATA);
+}
