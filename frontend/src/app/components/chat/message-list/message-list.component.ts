60fad64a5f0aab65f2ae0ad874a78ad93ff57929
Ewan Jones <egcjones@gmail.com>
2024-04-06 12:30:03 +0200

    add icebreakers to empty message list

--- frontend/src/app/components/chat/message-list/message-list.component.ts
+++ frontend/src/app/components/chat/message-list/message-list.component.ts
@@ -2,19 +2,28 @@ import { Component, Input } from '@angular/core';
 
 import { Message } from '@app/interfaces/message';
 
+import { IcebreakersComponent } from '../icebreakers/icebreakers.component';
 import { MessageListItemComponent } from '../message-list-item/message-list-item.component';
 
 @Component({
   selector: 'app-message-list',
   standalone: true,
-  imports: [MessageListItemComponent],
+  imports: [MessageListItemComponent, IcebreakersComponent],
   template: `
     <div class="message-list-wrapper">
       @for (message of messages; track message) {
         <app-message-list-item [message]="message"></app-message-list-item>
       } @empty {
-        <div class="empty-message">
-          <p>No messages yet</p>
+        <div
+          class="flex h-full flex-col items-center justify-between lg:mt-auto lg:h-1/2"
+        >
+          <div
+            class="prose flex flex-col items-center justify-center gap-4 text-center prose-headings:m-0"
+          >
+            <h1>👋</h1>
+            <h3>You're using Cognos secure AI messaging</h3>
+          </div>
+          <app-icebreakers></app-icebreakers>
         </div>
       }
     </div>
