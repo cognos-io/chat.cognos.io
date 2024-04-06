a0e02ba69b163d16466b7552ee812b909d913e1a
Ewan Jones <egcjones@gmail.com>
2024-04-06 13:39:25 +0200

    update the icebreakers and emit when one is selected

--- frontend/src/app/components/chat/icebreakers/icebreakers.component.ts
+++ frontend/src/app/components/chat/icebreakers/icebreakers.component.ts
@@ -1,5 +1,5 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, inject, signal } from '@angular/core';
+import { Component, EventEmitter, Output, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 interface IceBreaker {
@@ -10,24 +10,46 @@ interface IceBreaker {
 
 const iceBreakers: IceBreaker[] = [
   {
-    title: 'Write a short story...',
-    description: 'about a tiny dog navigating the big bad world of finance',
-    prompt: 'about a tiny dog navigating the big bad world of finance',
+    title: 'Future Tech Innovator',
+    description: 'envisioning the next big technology innovation',
+    prompt:
+      "Imagine you're a tech innovator in the year 2040. Describe a groundbreaking technology you've invented, how it works, and its impact on society.",
   },
   {
-    title: 'Write a short story...',
-    description: 'about a tiny dog navigating the big bad world of finance',
-    prompt: 'about a tiny dog navigating the big bad world of finance',
+    title: 'Eco-Friendly Earth',
+    description: 'designing a sustainable future for our planet',
+    prompt:
+      'Envision a future where Earth is completely eco-friendly. Describe three innovative changes or inventions that have helped achieve this sustainable lifestyle.',
   },
   {
-    title: 'Write a short story...',
-    description: 'about a tiny dog navigating the big bad world of finance',
-    prompt: 'about a tiny dog navigating the big bad world of finance',
+    title: "Space explorer's journal",
+    description: 'detail the chronicles of a space explorer in a distant galaxy',
+    prompt:
+      "You're a space explorer documenting your journey through a newly discovered galaxy. Describe your most fascinating discovery and how it changes our understanding of the universe.",
   },
   {
-    title: 'Write a short story...',
-    description: 'about a tiny dog navigating the big bad world of finance',
-    prompt: 'about a tiny dog navigating the big bad world of finance',
+    title: 'The hidden talent show',
+    description: 'to share and discover hidden talents',
+    prompt:
+      'Everyone has a hidden talent. Describe yours and how you discovered it. Alternatively, if you could instantly gain any talent, what would it be and why?',
+  },
+  {
+    title: 'The Ultimate Playlist',
+    description: 'Creating the perfect music playlist',
+    prompt:
+      "Compile the ultimate playlist for a road trip. Choose five songs, describe the mood they create, and why they're essential for your journey.",
+  },
+  {
+    title: 'Be an Ethereum developer',
+    description: 'optimized for smart contract development',
+    prompt:
+      'Imagine you are an experienced Ethereum developer tasked with creating a smart contract for a blockchain messenger. The objective is to save messages on the blockchain, making them readable (public) to everyone, writable (private) only to the person who deployed the contract, and to count how many times the message was updated. Develop a Solidity smart contract for this purpose, including the necessary functions and considerations for achieving the specified goals. Please provide the code and any relevant explanations to ensure a clear understanding of the implementation.',
+  },
+  {
+    title: 'Help me recruit',
+    description: 'a new member for my team',
+    prompt:
+      'I want you to act as a recruiter. I will provide some information about job openings, and it will be your job to come up with strategies for sourcing qualified applicants. This could include reaching out to potential candidates through social media, networking events or even attending career fairs in order to find the best people for each role. My first request is "I need help improve my CV.”',
   },
 ];
 
@@ -40,7 +62,7 @@ const iceBreakers: IceBreaker[] = [
       <li class="border-1 col-span-1 rounded-lg bg-white shadow">
         <button
           class="prose rounded-lg px-6 py-4 text-left"
-          (click)="onIceBreakerClick(ib)"
+          (click)="iceBreakerSelected.emit(ib)"
         >
           <h2>{{ ib.title }}</h2>
           <p>{{ ib.description }}</p>
@@ -54,9 +76,7 @@ export class IcebreakersComponent {
   public readonly iceBreakers = signal<IceBreaker[]>([]);
   private readonly _breakpointObserver = inject(BreakpointObserver);
 
-  onIceBreakerClick(iceBreaker: IceBreaker) {
-    console.log(iceBreaker.prompt);
-  }
+  @Output() iceBreakerSelected = new EventEmitter<IceBreaker>();
 
   constructor() {
     this._breakpointObserver
