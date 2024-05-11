71224a34453248cd50822100f6a1d5943deab881
Ewan Jones <egcjones@gmail.com>
2024-05-11 19:57:30 +0200

    clean up html and scss files

--- frontend/src/app/components/chat/conversation-detail/conversation-detail.component.ts
+++ frontend/src/app/components/chat/conversation-detail/conversation-detail.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, inject, viewChild } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { tap } from 'rxjs';
 
 import { ConversationService } from '@app/services/conversation.service';
 import { MessageService } from '@app/services/message.service';
@@ -11,16 +14,53 @@ import { MessageListComponent } from '../message-list/message-list.component';
   selector: 'app-conversation-detail',
   standalone: true,
   imports: [CommonModule, MessageFormComponent, MessageListComponent],
-  templateUrl: './conversation-detail.component.html',
-  styleUrl: './conversation-detail.component.scss',
+  template: `<div class="conversation-container w-full">
+    <app-message-list
+      class="message-container"
+      [messages]="(messageService.messages$ | async) ?? []"
+    ></app-message-list>
+    <app-message-form></app-message-form>
+  </div>`,
+  styles: `
+    .conversation-container {
+      display: flex;
+      flex-direction: column;
+
+      padding: var(--chat-container-padding);
+
+      height: 100%;
+    }
+
+    .message-container {
+      // needed for scrolling
+      height: 0px;
+      min-height: 100px;
+
+      flex-grow: 1;
+      overflow-y: auto;
+      overflow-x: hidden;
+    }
+  `,
 })
 export class ConversationDetailComponent {
   private readonly _conversationService = inject(ConversationService);
+  private readonly _messageListEl = viewChild(MessageListComponent);
 
   readonly messageService = inject(MessageService);
 
   @Input()
   set conversationId(conversationId: string) {
     this._conversationService.selectConversation$.next(conversationId ?? '');
+  }
+
+  constructor() {
+    this.messageService.messages$
+      .pipe(
+        takeUntilDestroyed(),
+        tap(() => console.log(this._messageListEl())),
+      )
+      .subscribe(() => {
+        this._messageListEl()?.scrollToBottom();
+      });
   }
 }
