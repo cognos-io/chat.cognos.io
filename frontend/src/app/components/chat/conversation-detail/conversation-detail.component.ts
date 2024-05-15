17c2e63ab65ac13e608494d8509518ab402d8cfe
Ewan Jones <egcjones@gmail.com>
2024-05-15 17:10:35 +0200

    showing loading indicator when fetching messages

--- frontend/src/app/components/chat/conversation-detail/conversation-detail.component.ts
+++ frontend/src/app/components/chat/conversation-detail/conversation-detail.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, inject, viewChild } from '@angular/core';
+import { Component, Input, computed, inject, viewChild } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
+import { LoadingIndicatorComponent } from '@app/components/loading-indicator/loading-indicator.component';
 import { ConversationService } from '@app/services/conversation.service';
-import { MessageService } from '@app/services/message.service';
+import { MessageService, MessageStatus } from '@app/services/message.service';
 
 import { MessageFormComponent } from '../message-form/message-form.component';
 import { MessageListComponent } from '../message-list/message-list.component';
@@ -11,13 +12,22 @@ import { MessageListComponent } from '../message-list/message-list.component';
 @Component({
   selector: 'app-conversation-detail',
   standalone: true,
-  imports: [CommonModule, MessageFormComponent, MessageListComponent],
+  imports: [
+    CommonModule,
+    MessageFormComponent,
+    MessageListComponent,
+    LoadingIndicatorComponent,
+  ],
   template: `<div class="conversation-container w-full">
-    <app-message-list
-      class="message-container"
-      [messages]="(messageService.messages$ | async) ?? []"
-    ></app-message-list>
-    <app-message-form></app-message-form>
+    @if (isFetching()) {
+      <app-loading-indicator></app-loading-indicator>
+    } @else {
+      <app-message-list
+        class="message-container"
+        [messages]="(messageService.messages$ | async) ?? []"
+      ></app-message-list>
+      <app-message-form></app-message-form>
+    }
   </div>`,
   styles: `
     .conversation-container {
@@ -45,6 +55,9 @@ export class ConversationDetailComponent {
   private readonly _messageListEl = viewChild(MessageListComponent);
 
   readonly messageService = inject(MessageService);
+  readonly isFetching = computed(
+    () => this.messageService.status() === MessageStatus.Fetching,
+  );
 
   @Input()
   set conversationId(conversationId: string) {
