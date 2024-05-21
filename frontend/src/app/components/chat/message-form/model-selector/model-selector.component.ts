e4fea2fb254a4dcb5c81967d727263df605751ce
Ewan Jones <egcjones@gmail.com>
2024-05-21 16:23:55 +0200

    add some text and a little button to flag which model is currently active

--- frontend/src/app/components/chat/message-form/model-selector/model-selector.component.ts
+++ frontend/src/app/components/chat/message-form/model-selector/model-selector.component.ts
@@ -13,18 +13,11 @@ import { ModelService } from '@app/services/model.service';
   imports: [MatDialogModule, MatButtonModule, MatRadioModule, FormsModule],
   template: ` <h2 mat-dialog-title>Pick your AI model</h2>
     <mat-dialog-content>
-      <h4>You are currently using:</h4>
-      <p>{{ selectedModel.name }}</p>
-      <p>{{ selectedModel.description }}</p>
-      @if (selectedModel.tags) {
-        <ul>
-          @for (tag of selectedModel.tags; track tag) {
-            <li>{{ tag }}</li>
-          }
-        </ul>
-      }
-      <hr />
-      <h4>Available models:</h4>
+      <p class="my-2">
+        You can choose from a variety of AI models to chat with. Each model has its own
+        unique personality and capabilities.
+      </p>
+      <h4 class="my-4 text-lg font-semibold">Available models:</h4>
 
       <ul role="list" class="divide-y divide-gray-100">
         <mat-radio-group required="true" [(ngModel)]="newModel">
@@ -34,6 +27,12 @@ import { ModelService } from '@app/services/model.service';
                 <div class="min-w-0 flex-auto">
                   <p class="text-sm font-semibold leading-6 text-gray-900">
                     {{ model.name }}
+                    @if (model.id === selectedModel.id) {
+                      <span
+                        class="mx-2 inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+                        >Currently active</span
+                      >
+                    }
                   </p>
                   <p class="text-gray-700">{{ model.description }}</p>
                   <p class="mt-1 flex text-xs leading-5 text-gray-500"></p>
@@ -48,6 +47,7 @@ import { ModelService } from '@app/services/model.service';
       <button mat-button mat-dialog-close>Cancel</button>
       <button
         mat-button
+        color="primary"
         [mat-dialog-close]="selectedModel"
         [disabled]="newModel === selectedModel"
         (click)="onSaveModelChange()"
