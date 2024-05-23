c3200dbac5da7a4d6a680796d3061cb3c1a1f8e8
Ewan Jones <egcjones@gmail.com>
2024-05-23 16:40:47 +0200

    feat: Add CognosLogoComponent

---
+++ frontend/src/app/components/cognos-logo/cognos-logo.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cognos-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: `./cognos_logo--horizontal.svg`,
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CognosLogoComponent {}
