d547c6b40cb49fa5ad6c6d3e2d3b7f40516b7ba8
Ewan Jones <egcjones@gmail.com>
2024-05-24 21:42:04 +0200

    login now redirects upon successful login

--- frontend/src/app/pages/auth/login/login.component.ts
+++ frontend/src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, computed, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+
+import { filterNil } from 'ngxtension/filter-nil';
 
 import { AuthService } from '../../../services/auth.service';
 
@@ -15,6 +19,15 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class LoginComponent {
   readonly authService: AuthService = inject(AuthService);
+  private readonly _router: Router = inject(Router);
 
   loading = computed(() => this.authService.status() === 'authenticating');
+
+  constructor() {
+    this.authService.user$.pipe(takeUntilDestroyed(), filterNil()).subscribe((user) => {
+      if (user) {
+        this._router.navigate(['/']);
+      }
+    });
+  }
 }
