a86b47eb2f5a9645cf7906a4ff250d4e681c2d09
Ewan Jones <egcjones@gmail.com>
2024-05-31 12:59:55 +0200

    set production pocketbase url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   isDevelopment: false, // Has to be explicitly enabled
-  pocketbaseBaseUrl: 'http://localhost:8090',
+  pocketbaseBaseUrl: 'https://api.pocketbase.io',
   localVaultPassword: '',
 };
