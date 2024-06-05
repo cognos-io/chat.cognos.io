6e7c82438f5790ee29d3c4c6785915d414ee16e2
Ewan Jones <egcjones@gmail.com>
2024-06-05 15:16:47 +0200

    run title creation and sending message at the same time

--- frontend/src/app/services/message.service.ts
+++ frontend/src/app/services/message.service.ts
@@ -8,6 +8,7 @@ import {
   Observable,
   Subject,
   catchError,
+  combineLatest,
   concatMap,
   exhaustMap,
   filter,
@@ -15,6 +16,7 @@ import {
   from,
   map,
   of,
+  startWith,
   switchMap,
   take,
   tap,
@@ -170,6 +172,7 @@ export class MessageService {
           if (!messageRequest.parentMessageId) {
             // Take the most recent message as the parent message
             const messages = this.state.orderedMessageList();
+
             const lastMessage = messages[messages.length - 1];
             if (lastMessage) {
               messageRequest.parentMessageId = lastMessage.record_id;
@@ -190,31 +193,36 @@ export class MessageService {
           this.state.addMessage(msg);
 
           const conversation = this._conversationService.conversation();
+
           if (!conversation) {
             this._isNewConversation$.next(true);
             this._conversationService.newConversation$.next({
               title: 'New Conversation',
             });
+
             return this._conversationService.conversation$.pipe(
               filterNil(),
               take(1),
               switchMap((newConversation) => {
-                return this.generateAndSetConversationTitle(
-                  newConversation.record.id,
-                  messageRequest.content,
-                );
-              }),
-              concatMap(() => {
-                return this.sendMessage(messageRequest).pipe(
-                  finalize(() => this._isNewConversation$.next(false)),
-                  tap((resp) => {
-                    const metadata: CognosMetadataResponse = resp.metadata?.cognos;
-                    this.state.updateMessageId({
-                      oldId: messageRequest.requestId,
-                      newId: metadata.message_record_id || '',
-                    });
-                  }),
-                  map((resp) => {
+                return combineLatest([
+                  // Generate a conversation title based on the first message
+                  this.generateAndSetConversationTitle(
+                    newConversation.record.id,
+                    messageRequest.content,
+                  ).pipe(startWith(newConversation)),
+                  // And send the message
+                  this.sendMessage(messageRequest).pipe(
+                    finalize(() => this._isNewConversation$.next(false)),
+                    tap((resp) => {
+                      const metadata: CognosMetadataResponse = resp.metadata?.cognos;
+                      this.state.updateMessageId({
+                        oldId: messageRequest.requestId,
+                        newId: metadata.message_record_id || '',
+                      });
+                    }),
+                  ),
+                ]).pipe(
+                  map(([, resp]) => {
                     return {
                       ...this.addOpenAIMessageToState(resp),
                     };
@@ -539,8 +547,6 @@ export class MessageService {
       return EMPTY;
     }
 
-    this.state.setStatus(MessageStatus.Sending);
-
     return from(
       this._openAi.chat.completions.create({
         max_tokens: 15,
@@ -554,9 +560,6 @@ export class MessageService {
         },
       }),
     ).pipe(
-      finalize(() => {
-        this.state.setStatus(MessageStatus.None);
-      }),
       catchError((err) => {
         console.error('Error generating conversation title', err);
         return EMPTY;
@@ -573,8 +576,8 @@ export class MessageService {
   ): Observable<ConversationRecord> {
     return this.generateConversationTitle(startingMessage).pipe(
       switchMap((title) => {
-        // Use max the first 5 words
-        title = title.split(' ').slice(0, 5).join(' ');
+        // Use max the first 10 words
+        title = title.split(' ').slice(0, 10).join(' ');
         return this._conversationService.editConversation(conversationId, { title });
       }),
     );
