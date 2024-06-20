3e1599ddfb26c6023f49dd576863fab06fa212dd
Ewan Jones <egcjones@gmail.com>
2024-06-20 15:10:52 +0200

    outputting models grouped by provider

--- frontend/src/app/components/chat/message-form/model-selector/model-selector.component.ts
+++ frontend/src/app/components/chat/message-form/model-selector/model-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -26,35 +26,38 @@ import { ModelService } from '@app/services/model.service';
       </p>
       <h4 class="my-4 text-lg font-semibold">Available models:</h4>
 
-      <ul role="list" class="divide-y divide-gray-100">
-        <mat-radio-group required="true" [(ngModel)]="newModel">
-          @for (model of modelService.modelList(); track model.id) {
-            <li class="flex items-center justify-between gap-x-6 py-5">
-              <mat-radio-button [value]="model" class="flex flex-grow gap-x-4">
-                <div class="min-w-0 flex-auto">
-                  @if (model.id === selectedModel.id) {
-                    <span
-                      class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
-                      >Currently active</span
-                    >
-                  }
-                  <p class="text-sm font-semibold leading-6 text-gray-900">
-                    {{ model.name }}
-                  </p>
-                  <p class="my-2 text-gray-700">{{ model.description }}</p>
-                  @if (model.tags && model.tags.length > 0) {
-                    <div class="mt-2 flex gap-x-2">
-                      @for (tag of model.tags; track tag) {
-                        <app-tag [tag]="tag"></app-tag>
-                      }
-                    </div>
-                  }
-                </div>
-              </mat-radio-button>
-            </li>
-          }
-        </mat-radio-group>
-      </ul>
+      @for (providerId of providerIds(); track providerId) {
+        <h5 class="text-sm font-semibold">{{ providerId }}</h5>
+        <ul role="list" class="divide-y divide-gray-100">
+          <mat-radio-group required="true" [(ngModel)]="newModel">
+            @for (model of modelService.groupedModels()[providerId]; track model.id) {
+              <li class="flex items-center justify-between gap-x-6 py-5">
+                <mat-radio-button [value]="model" class="flex flex-grow gap-x-4">
+                  <div class="min-w-0 flex-auto">
+                    @if (model.id === selectedModel.id) {
+                      <span
+                        class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+                        >Currently active</span
+                      >
+                    }
+                    <p class="text-sm font-semibold leading-6 text-gray-900">
+                      {{ model.name }}
+                    </p>
+                    <p class="my-2 text-gray-700">{{ model.description }}</p>
+                    @if (model.tags && model.tags.length > 0) {
+                      <div class="mt-2 flex gap-x-2">
+                        @for (tag of model.tags; track tag) {
+                          <app-tag [tag]="tag"></app-tag>
+                        }
+                      </div>
+                    }
+                  </div>
+                </mat-radio-button>
+              </li>
+            }
+          </mat-radio-group>
+        </ul>
+      }
     </mat-dialog-content>
     <mat-dialog-actions align="end">
       <button mat-button mat-dialog-close>Cancel</button>
@@ -74,6 +77,8 @@ export class ModelSelectorComponent {
   readonly modelService = inject(ModelService);
 
   newModel: Model = this.modelService.selectedModel();
+
+  providerIds = computed(() => Object.keys(this.modelService.groupedModels()));
 
   get selectedModel() {
     return this.modelService.selectedModel();
