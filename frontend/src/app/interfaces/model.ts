dfac313dae36088039985faf2f754901437da162
Ewan Jones <egcjones@gmail.com>
2024-06-20 14:53:41 +0200

    add a provider id to models to help with grouping

--- frontend/src/app/interfaces/model.ts
+++ frontend/src/app/interfaces/model.ts
@@ -6,6 +6,7 @@ export const Model = z.object({
   id: z.string(),
   name: z.string(),
   slug: z.string(),
+  providerId: z.string(),
   description: z.string(),
   tags: z.array(Tag).optional(),
   inputContextLength: z.number(), // How many tokens can be passed to the model
@@ -18,31 +19,33 @@ export type Model = z.infer<typeof Model>;
 // for whatever reason.
 const anthropicClaudeHaikuModel: Model = {
   id: 'anthropic:claude-haiku',
-  name: 'Anthropic - Claude Haiku',
+  name: 'Claude Haiku',
+  providerId: 'anthropic',
   slug: 'anthropic---claude-haiku',
   description: "Anthropic's Claude Haiku is their fastest general purpose model.",
   inputContextLength: 200_000,
-  tags: [{ title: 'anthropic' }, { title: 'fast' }],
+  tags: [{ title: 'fast' }],
 };
 const anthropicClaudeSonnetModel: Model = {
   id: 'anthropic:claude-sonnet',
-  name: 'Anthropic - Claude Sonnet',
+  name: 'Claude Sonnet',
+  providerId: 'anthropic',
   slug: 'anthropic---claude-sonnet',
   description:
     "Anthropic's Claude Sonnet is a mid level general purpose model balancing speed and intelligence.",
   inputContextLength: 200_000,
-  tags: [{ title: 'anthropic' }, { title: 'general-purpose' }],
+  tags: [{ title: 'general-purpose' }],
 };
 
 const anthropicClaudeOpusModel: Model = {
   id: 'anthropic:claude-opus',
-  name: 'Anthropic - Claude Opus',
+  name: 'Claude Opus',
+  providerId: 'anthropic',
   slug: 'anthropic---claude-opus',
   description:
     "Anthropic's Claude Opus is an advanced intelligence general purpose model.",
   inputContextLength: 200_000,
   tags: [
-    { title: 'anthropic' },
     { title: 'general-purpose' },
     { title: 'high-intelligence' },
     { title: 'slow' },
@@ -51,48 +54,42 @@ const anthropicClaudeOpusModel: Model = {
 
 const openAiGpt35TurboModel: Model = {
   id: 'openai:gpt-3.5-turbo',
-  name: 'Open AI - GPT 3.5 Turbo',
+  name: 'GPT 3.5 Turbo',
+  providerId: 'openai',
   slug: 'open-ai---gpt-35-turbo',
   description: "OpenAI's fast, inexpensive model for general-purpose use.",
   inputContextLength: 16_385,
-  tags: [{ title: 'openai' }, { title: 'general-purpose' }, { title: 'fast' }],
+  tags: [{ title: 'general-purpose' }, { title: 'fast' }],
 };
 
 const openAiGpt4oModel: Model = {
   id: 'openai:gpt-4o',
-  name: 'Open AI - GPT 4 Omni',
+  name: 'GPT 4 Omni',
+  providerId: 'openai',
   slug: 'open-ai---gpt-4o',
   description: "OpenAI's GPT 4 Omni (GPT4o) model",
   inputContextLength: 128_000,
-  tags: [
-    { title: 'openai' },
-    { title: 'general-purpose' },
-    { title: 'high-intelligence' },
-  ],
+  tags: [{ title: 'general-purpose' }, { title: 'high-intelligence' }],
 };
 
 const googleGemini15FlashModel: Model = {
   id: 'google:gemini-1.5-flash',
-  name: 'Google - Gemini 1.5 Flash',
+  name: 'Gemini 1.5 Flash',
+  providerId: 'google',
   slug: 'google---gemini-15-flash',
   description: `Google's Gemini 1.5 Flash model is a fast, general-purpose model with a long context.`,
   inputContextLength: 1_048_576,
-  tags: [
-    { title: 'google' },
-    { title: 'general-purpose' },
-    { title: 'long-context' },
-    { title: 'fast' },
-  ],
+  tags: [{ title: 'general-purpose' }, { title: 'long-context' }, { title: 'fast' }],
 };
 
 const googleGemini15ProModel: Model = {
   id: 'google:gemini-1.5-pro',
-  name: 'Google - Gemini 1.5 Pro',
+  name: 'Gemini 1.5 Pro',
+  providerId: 'google',
   slug: 'google---gemini-15-pro',
   description: `Google's Gemini 1.5 Pro is an advanced, general purpose model with a long context. It is slower than the Flash model but has higher intelligence.`,
   inputContextLength: 1_048_576,
   tags: [
-    { title: 'google' },
     { title: 'general-purpose' },
     { title: 'long-context' },
     { title: 'high-intelligence' },
@@ -101,7 +98,8 @@ const googleGemini15ProModel: Model = {
 
 const deepInfraOpenChat368B: Model = {
   id: 'deepinfra:openchat-3.6-8b',
-  name: 'DeepInfra - OpenChat 3.6 8B',
+  name: 'OpenChat 3.6 8B',
+  providerId: 'deepinfra',
   slug: 'deepinfra---openchat-36-8b',
   description:
     'OpenChat is a LLama-3-8B fine-tune that outperforms it on multiple benchmarks.',
@@ -110,7 +108,8 @@ const deepInfraOpenChat368B: Model = {
 };
 const deepInfraWizardLm28x22b: Model = {
   id: 'deepinfra:wizardlm-2-8x22b',
-  name: 'DeepInfra - WizardLM-2 8x22B',
+  name: 'WizardLM-2 8x22B',
+  providerId: 'deepinfra',
   slug: 'deepinfra---wizardlm-2-8x22b',
   description:
     'Developed at Microsoft, WizardLM-2 is a mixture of experts model that extends Mixtral-8x22B and is capable of general-purpose tasks.',
@@ -124,7 +123,8 @@ const deepInfraWizardLm28x22b: Model = {
 };
 const deepInfraGemma117bit: Model = {
   id: 'deepinfra:gemma-1.1-7b-it',
-  name: 'DeepInfra - Gemma 1.1 7B IT',
+  name: 'Gemma 1.1 7B IT',
+  providerId: 'deepinfra',
   slug: 'deepinfra---gemma-11-7b-it',
   description:
     'Developed by Google, Gemma is an open-source model that leverages the same research and technology as Google Gemini models.',
@@ -133,7 +133,8 @@ const deepInfraGemma117bit: Model = {
 };
 const deepInfraDolphin26mixtral8x7b: Model = {
   id: 'deepinfra:dolphin-2.6-mixtral-8x7b',
-  name: 'DeepInfra - Dolphin 2.6 Mixtral 8x7B',
+  name: 'Dolphin 2.6 Mixtral 8x7B',
+  providerId: 'deepinfra',
   slug: 'deepinfra---dolphin-26-mixtral-8x7b',
   description:
     'Dolphin is an uncensored model that is capable of general-purpose and coding tasks.',
@@ -147,7 +148,8 @@ const deepInfraDolphin26mixtral8x7b: Model = {
 };
 const deepInfraChronosHermes13bv2: Model = {
   id: 'deepinfra:chronos-hermes-13b-v2',
-  name: 'DeepInfra - Chronos Hermes 13B v2',
+  name: 'Chronos Hermes 13B v2',
+  providerId: 'deepinfra',
   slug: 'deepinfra---chronos-hermes-13b-v2',
   description:
     'Optimized for creative writing tasks, Chronos Hermes is focused on chat, role play and story writing, with good reasoning and logic.',
@@ -156,7 +158,8 @@ const deepInfraChronosHermes13bv2: Model = {
 };
 const deepInfraPhindCodeLlama34bv2: Model = {
   id: 'deepinfra:phind-codellama-34b-v2',
-  name: 'DeepInfra - Phind CodeLLama 34B v2',
+  name: 'Phind CodeLLama 34B v2',
+  providerId: 'deepinfra',
   slug: 'deepinfra---phind-codellama-34b-v2',
   description:
     'Phind CodeLLama is a model optimized for coding tasks and performs well with multiple programming languages including Python, C/C++, TypeScript, Java.',
@@ -165,7 +168,8 @@ const deepInfraPhindCodeLlama34bv2: Model = {
 };
 const deepInfraCodeGemma7bit: Model = {
   id: 'deepinfra:codegemma-7b-it',
-  name: 'DeepInfra - CodeGemma 7B IT',
+  name: 'CodeGemma 7B IT',
+  providerId: 'deepinfra',
   slug: 'deepinfra---codegemma-7b-it',
   description:
     'This model is intended to answer questions about code fragments, to generate code from natural language, or to engage in a conversation with the user about programming or technical problems.',
@@ -174,7 +178,8 @@ const deepInfraCodeGemma7bit: Model = {
 };
 const deepInfraLlama38binstruct: Model = {
   id: 'deepinfra:llama-3-8b-instruct',
-  name: 'DeepInfra - LLama 3 8B Instruct',
+  name: 'LLama 3 8B Instruct',
+  providerId: 'deepinfra',
   slug: 'deepinfra---llama-3-8b-instruct',
   description: `Meta's open source LLama3 8B model hosted on the DeepInfra infrastructure`,
   inputContextLength: 8_192,
@@ -182,7 +187,8 @@ const deepInfraLlama38binstruct: Model = {
 };
 const deepInfraLzlv70bfp16hf: Model = {
   id: 'deepinfra:lzlv_70b_fp16_hf',
-  name: 'DeepInfra - LZLV 70B FP16 HF',
+  name: 'LZLV 70B FP16 HF',
+  providerId: 'deepinfra',
   slug: 'deepinfra---lzlv-70b-fp16-hf',
   description:
     'A mix of models focused on creative writing such as role playing and story telling.',
@@ -196,7 +202,8 @@ const deepInfraLzlv70bfp16hf: Model = {
 
 const cloudflareLlama38bInstruct: Model = {
   id: 'cloudflare:llama-3-8b-instruct',
-  name: 'Cloudflare - Llama3 8B Instruct',
+  name: 'Llama3 8B Instruct',
+  providerId: 'cloudflare',
   slug: 'cloudflare---llama3-8b-instruct',
   description: `Meta's open source LLama3 8B model hosted on the Cloudflare Workers AI infrastructure`,
   inputContextLength: 8_192,
@@ -205,7 +212,8 @@ const cloudflareLlama38bInstruct: Model = {
 
 const cloudflareQwen157BChat: Model = {
   id: 'cloudflare:qwen-15-7b-chat',
-  name: 'Cloudflare - Qwen 1.5 7B Chat',
+  name: 'Qwen 1.5 7B Chat',
+  providerId: 'cloudflare',
   slug: 'cloudflare---qwen-15-7b-chat',
   description: `Qwen's Qwen 1.5 7B Chat model hosted on the Cloudflare Workers AI infrastructure`,
   inputContextLength: 32_768,
@@ -219,7 +227,8 @@ const cloudflareQwen157BChat: Model = {
 
 const cloudflareMistral7bInstruct: Model = {
   id: 'cloudflare:mistral-7b-instruct-v0.2',
-  name: 'Cloudflare - Mistral 7B Instruct v0.2',
+  name: 'Mistral 7B Instruct v0.2',
+  providerId: 'cloudflare',
   slug: 'cloudflare---mistral-7b-instruct-v0.2',
   description: `Mistral's Mistral 7B Instruct v0.2 model hosted on the Cloudflare Workers AI infrastructure`,
   inputContextLength: 32_768,
@@ -228,7 +237,8 @@ const cloudflareMistral7bInstruct: Model = {
 
 const cloudflareDeepseekMath7bInstruct: Model = {
   id: 'cloudflare:deepseek-math-7b-instruct',
-  name: 'Cloudflare - Deepseek Math 7B Instruct',
+  name: 'Deepseek Math 7B Instruct',
+  providerId: 'cloudflare',
   slug: 'cloudflare---deepseek-math-7b-instruct',
   description: `Deepseek AI's Deepseek Math 7B Instruct model hosted on the Cloudflare Workers AI infrastructure`,
   inputContextLength: 4_096,
