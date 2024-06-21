3baa4c4b2c41de5a41fff237e4f8dc3d5b227e68
Ewan Jones <egcjones@gmail.com>
2024-06-21 07:35:08 +0200

    outlining some styles for bento list

--- frontend/src/app/pages/chat/chat.component.ts
+++ frontend/src/app/pages/chat/chat.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  OnDestroy,
-  computed,
-  effect,
-  inject,
-  viewChild,
-} from '@angular/core';
+import { Component, OnDestroy, computed, inject, viewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
