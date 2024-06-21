0d007b17791ea163f70b585121588580e8cc8d79
Ewan Jones <egcjones@gmail.com>
2024-06-21 16:41:18 +0200

    moving conversation list item to individual component

---
+++ frontend/src/app/components/chat/conversation-list/conversation-list-item/conversation-list-item.component.ts
@@ -0,0 +1,172 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnDestroy,
+  inject,
+} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router, RouterModule } from '@angular/router';
+
+import { Subject, takeUntil } from 'rxjs';
+
+import { ConfirmationDialogComponent } from '@app/components/confirmation-dialog/confirmation-dialog.component';
+import { EditConversationDialogComponent } from '@app/components/edit-conversation-dialog/edit-conversation-dialog.component';
+import { Conversation } from '@app/interfaces/conversation';
+import { ConversationService } from '@app/services/conversation.service';
+import { UserPreferencesService } from '@app/services/user-preferences.service';
+
+@Component({
+  selector: 'app-conversation-list-item',
+  standalone: true,
+  imports: [
+    RouterModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatIconModule,
+    CommonModule,
+    MatListModule,
+  ],
+  template: `<a
+      mat-list-item
+      [routerLink]="['c', conversation.record.id]"
+      class="conversation-list-item group"
+      [matTooltip]="conversation.decryptedData.title"
+      matTooltipPosition="after"
+    >
+      <p matListItemTitle>{{ conversation.decryptedData.title }}</p>
+      <div
+        matListItemMeta
+        class="conversation-list-item-actions hidden group-hover:flex group-active:flex"
+      >
+        <button
+          mat-icon-button
+          class="actions-button"
+          [matMenuTriggerFor]="menu"
+          aria-label="Open conversation menu"
+        >
+          <mat-icon fontSet="bi" fontIcon="bi-three-dots-vertical"></mat-icon>
+        </button>
+      </div>
+    </a>
+    <mat-menu #menu="matMenu">
+      <button mat-menu-item="" (click)="onPinUnpinConversation(conversation.record.id)">
+        <mat-icon
+          [ngClass]="{
+            pinned: isConversationPinned(conversation.record.id)
+          }"
+          fontSet="bi"
+          [fontIcon]="
+            isConversationPinned(conversation.record.id)
+              ? 'bi-pin-angle-fill'
+              : 'bi-pin-angle'
+          "
+        />
+        @if (isConversationPinned(conversation.record.id)) {
+          Unpin
+        } @else {
+          Pin
+        }
+      </button>
+      <button mat-menu-item="" (click)="onRenameConversation(conversation.record.id)">
+        <mat-icon fontSet="bi" fontIcon="bi-pencil-square"></mat-icon>
+        Edit
+      </button>
+      <button mat-menu-item="" (click)="onDeleteConversation(conversation.record.id)">
+        <mat-icon fontSet="bi" fontIcon="bi-trash3"></mat-icon>
+        Delete
+      </button>
+    </mat-menu> `,
+  styles: `
+    @import 'include-media/dist/include-media';
+
+    @include media('>=tablet') {
+      .conversation-list-item-actions {
+        opacity: 0;
+      }
+
+      .conversation-list-item {
+        &:hover {
+          .conversation-list-item-actions {
+            opacity: 1;
+          }
+        }
+      }
+    }
+
+    .conversation-list-item {
+      transition: all 150ms ease-in;
+    }
+
+    .conversation-list-item-actions {
+      flex-direction: column;
+
+      .actions-button {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+
+    mat-icon.pinned {
+      @apply text-green-700;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConversationListItemComponent implements OnDestroy {
+  @Input({ required: true }) conversation!: Conversation;
+
+  private readonly _preferencesService = inject(UserPreferencesService);
+  private readonly _destroyed$ = new Subject<void>();
+  private readonly _dialog = inject(MatDialog);
+  readonly router = inject(Router);
+  private readonly _conversationService = inject(ConversationService);
+
+  ngOnDestroy(): void {
+    this._destroyed$.next();
+    this._destroyed$.complete();
+  }
+
+  onRenameConversation(conversationId: string) {
+    this._dialog.open(EditConversationDialogComponent, {
+      data: {
+        conversationId,
+      },
+    });
+  }
+
+  onDeleteConversation(conversationId: string) {
+    this._dialog
+      .open(ConfirmationDialogComponent, {
+        data: {
+          message: 'Are you sure you want to delete this conversation?',
+        },
+      })
+      .afterClosed()
+      .pipe(takeUntil(this._destroyed$))
+      .subscribe((result: boolean) => {
+        if (result) {
+          this._conversationService.deleteConversation$.next(conversationId);
+          this.router.navigate(['/']);
+        }
+      });
+  }
+
+  isConversationPinned(conversationId: string) {
+    return this._preferencesService.isConversationPinned(conversationId);
+  }
+
+  onPinUnpinConversation(conversationId: string) {
+    if (this.isConversationPinned(conversationId)) {
+      this._preferencesService.unpinConversation(conversationId);
+    } else {
+      this._preferencesService.pinConversation(conversationId);
+    }
+  }
+}
--- frontend/src/app/pages/chat/chat.component.ts
+++ frontend/src/app/pages/chat/chat.component.ts
@@ -1,24 +1,19 @@
-import { Component, OnDestroy, computed, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Router, RouterModule } from '@angular/router';
 
-import { Subject, takeUntil } from 'rxjs';
-
+import { ConversationListItemComponent } from '@app/components/chat/conversation-list/conversation-list-item/conversation-list-item.component';
 import { CognosLogoComponent } from '@app/components/cognos-logo/cognos-logo.component';
-import { ConfirmationDialogComponent } from '@app/components/confirmation-dialog/confirmation-dialog.component';
 import { ContactHelpDialogComponent } from '@app/components/contact-help-dialog/contact-help-dialog.component';
-import { EditConversationDialogComponent } from '@app/components/edit-conversation-dialog/edit-conversation-dialog.component';
 import { DeviceService } from '@app/services/device.service';
 import { MessageService } from '@app/services/message.service';
-import { UserPreferencesService } from '@app/services/user-preferences.service';
 
 import { ConversationService } from '../../services/conversation.service';
 import { VaultService } from '../../services/vault.service';
@@ -36,20 +31,17 @@ import { VaultService } from '../../services/vault.service';
     MatListModule,
     MatTooltipModule,
     MatExpansionModule,
-    MatMenuModule,
     CognosLogoComponent,
+    ConversationListItemComponent,
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
-export class ChatComponent implements OnDestroy {
-  private readonly _destroyed$ = new Subject<void>();
-
-  private readonly dialog = inject(MatDialog);
+export class ChatComponent {
   private readonly _deviceService = inject(DeviceService);
   private readonly _conversationService = inject(ConversationService);
   private readonly _messageService = inject(MessageService);
-  private readonly _preferencesService = inject(UserPreferencesService);
+  private readonly _dialog = inject(MatDialog);
 
   readonly router = inject(Router);
   readonly conversationService = inject(ConversationService);
@@ -64,13 +56,8 @@ export class ChatComponent implements OnDestroy {
     );
   });
 
-  ngOnDestroy(): void {
-    this._destroyed$.next();
-    this._destroyed$.complete();
-  }
-
   onOpenHelpDialog() {
-    this.dialog.open(ContactHelpDialogComponent);
+    this._dialog.open(ContactHelpDialogComponent);
   }
 
   onNewConversation() {
@@ -80,43 +67,6 @@ export class ChatComponent implements OnDestroy {
     // don't navigate if we're already on the new conversation page
     if (this.router.url !== '/') {
       this.router.navigateByUrl('/');
-    }
-  }
-
-  onRenameConversation(conversationId: string) {
-    this.dialog.open(EditConversationDialogComponent, {
-      data: {
-        conversationId,
-      },
-    });
-  }
-
-  onDeleteConversation(conversationId: string) {
-    this.dialog
-      .open(ConfirmationDialogComponent, {
-        data: {
-          message: 'Are you sure you want to delete this conversation?',
-        },
-      })
-      .afterClosed()
-      .pipe(takeUntil(this._destroyed$))
-      .subscribe((result: boolean) => {
-        if (result) {
-          this.conversationService.deleteConversation$.next(conversationId);
-          this.router.navigate(['/']);
-        }
-      });
-  }
-
-  isConversationPinned(conversationId: string) {
-    return this._preferencesService.isConversationPinned(conversationId);
-  }
-
-  onPinUnpinConversation(conversationId: string) {
-    if (this.isConversationPinned(conversationId)) {
-      this._preferencesService.unpinConversation(conversationId);
-    } else {
-      this._preferencesService.pinConversation(conversationId);
     }
   }
 
