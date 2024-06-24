609e7dd2e752aff910b6d84a21134fd954636d4c
Ewan Jones <egcjones@gmail.com>
2024-06-24 21:39:11 +0200

    adding more checks for empty content

--- frontend/src/app/components/chat/message-list-item/message-list-item.component.ts
+++ frontend/src/app/components/chat/message-list-item/message-list-item.component.ts
@@ -61,9 +61,16 @@ import { ModelService } from '@app/services/model.service';
           >
         </div>
         <article class="item-content prose prose-headings:text-xl prose-th:text-base">
-          <markdown emoji>
-            {{ message.decryptedData.content }}
-          </markdown>
+          @if (message.decryptedData.content) {
+            <markdown emoji>
+              {{ message.decryptedData.content }}
+            </markdown>
+          } @else {
+            <p i18n class="italic text-gray-500">
+              This message is empty or the AI did not generate a response, please try
+              again.
+            </p>
+          }
         </article>
         <div class="item-content prose flex gap-2 opacity-0 group-hover:opacity-100">
           @if (message.decryptedData.content) {
