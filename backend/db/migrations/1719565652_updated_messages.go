27833a9c4b1ed5342a83e82a16fcdf29c714cf06
Ewan Jones <egcjones@gmail.com>
2024-06-28 11:09:19 +0200

    remove cascade delete from parent message id

---
+++ backend/db/migrations/1719565652_updated_messages.go
@@ -0,0 +1,76 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("v893vvhgp688kie")
+		if err != nil {
+			return err
+		}
+
+		// update
+		edit_parent_message := &schema.SchemaField{}
+		if err := json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "nciypmmv",
+			"name": "parent_message",
+			"type": "relation",
+			"required": false,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"collectionId": "v893vvhgp688kie",
+				"cascadeDelete": false,
+				"minSelect": null,
+				"maxSelect": 1,
+				"displayFields": null
+			}
+		}`), edit_parent_message); err != nil {
+			return err
+		}
+		collection.Schema.AddField(edit_parent_message)
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("v893vvhgp688kie")
+		if err != nil {
+			return err
+		}
+
+		// update
+		edit_parent_message := &schema.SchemaField{}
+		if err := json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "nciypmmv",
+			"name": "parent_message",
+			"type": "relation",
+			"required": false,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"collectionId": "v893vvhgp688kie",
+				"cascadeDelete": true,
+				"minSelect": null,
+				"maxSelect": 1,
+				"displayFields": null
+			}
+		}`), edit_parent_message); err != nil {
+			return err
+		}
+		collection.Schema.AddField(edit_parent_message)
+
+		return dao.SaveCollection(collection)
+	})
+}
