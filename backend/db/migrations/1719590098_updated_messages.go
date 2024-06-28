23caf7c093547887213b11f1a15880f3f6ce164d
Ewan Jones <egcjones@gmail.com>
2024-06-28 18:13:35 +0200

    add migrations to fix duration values in conversation and add expiry field to messages

---
+++ backend/db/migrations/1719590098_updated_messages.go
@@ -0,0 +1,54 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("v893vvhgp688kie")
+		if err != nil {
+			return err
+		}
+
+		// add
+		new_expires := &schema.SchemaField{}
+		if err := json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "xuxxmxiy",
+			"name": "expires",
+			"type": "date",
+			"required": false,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"min": "2024-06-01 12:00:00.000Z",
+				"max": ""
+			}
+		}`), new_expires); err != nil {
+			return err
+		}
+		collection.Schema.AddField(new_expires)
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("v893vvhgp688kie")
+		if err != nil {
+			return err
+		}
+
+		// remove
+		collection.Schema.RemoveField("xuxxmxiy")
+
+		return dao.SaveCollection(collection)
+	})
+}
